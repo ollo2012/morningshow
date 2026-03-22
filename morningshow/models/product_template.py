22ff3d98b54c14f72f1df0db6793b59e57d60d72
Niels Göttsch <ng@ife.de>
2026-03-22 12:01:28 +0100

    [IMP] fix api header and image sending

--- morningshow/models/product_template.py
+++ morningshow/models/product_template.py
@@ -2,21 +2,24 @@ import requests
 import json
 from odoo import models
 from odoo.exceptions import UserError
+import base64
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def action_send_to_morningshow(self):
+        self.ensure_one()
         # Your Morningshow API URL
         url = "http://localhost:3000/api/products"
         # Data to send
         payload = {
             "title": self.name,
             "text": self.description_sale or "",
+            "image": self.image_1920.decode('utf-8') if self.image_1920 else "",
         }
         headers = {
             'Content-Type': 'application/json',
-            'x-sync-token': 'sync-token-123'
+            'x-api-token': 'sync-token-123'
         }
         try:
             response = requests.post(url, data=json.dumps(payload), headers=headers, timeout=10)
